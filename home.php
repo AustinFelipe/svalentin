d13f3d1f6a5e09c9d3aaeb069588121c0c21077b
Austin Felipe <austinfelipe@ig.com.br>
2015-04-01 03:55:15 +1100

    Incluido js para tratar passagem de fotos

--- home.php
+++ home.php
@@ -1,7 +1,8 @@
 <?php
     $page_title = 'Home';
     $page_canonical = '';
-    $billboard_image = 'img/billboard.jpg';
+    $billboard_image = '';
     $body = 'partials/home_menu.php';
+    $scripts = 'js/slider.js';
     include('templates/_layout.php');
 ?>
