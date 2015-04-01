0438eb29b42c076bbbbab5d146955b3b5f1caafa
Austin Felipe <austinfelipe@ig.com.br>
2015-04-02 05:12:27 +1100

    Adicionado imagens da gallery

--- templates/gallery.php
+++ templates/gallery.php
@@ -6,7 +6,8 @@
 </div>
 
 <div class="container">
-    <div class="center-block">
+    <div class="center-block ultralb">
+        <img class="img-responsive" src="img/gallery.jpg" alt="San Valentino" no-show="" />
         <img class="img-responsive" src="img/gallery.jpg" alt="San Valentino" />
     </div>
 </div>
